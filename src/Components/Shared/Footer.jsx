468c23f8ef39eaaf438578ae43155025c0ff8912
RoufRain <roufrain91@gmail.com>
2023-11-08 11:19:28 +0600

    added footer which will share multiple pages

---
+++ src/Components/Shared/Footer.jsx
@@ -0,0 +1,38 @@
+import logo from "../../assets/images/hotel-logo-design.gif";
+
+const Footer = () => {
+  return (
+    <footer className="footer p-10 bg-base-200 text-base-content">
+      <aside>
+        <img src={logo} alt="" className="h-24 w-52" />
+        <p>
+          Hotel Golden House Ltd.
+          <br />
+          Providing reliable services since 2000
+        </p>
+      </aside>
+      <nav>
+        <header className="footer-title">Services</header>
+        <a className="link link-hover">Branding</a>
+        <a className="link link-hover">Design</a>
+        <a className="link link-hover">Marketing</a>
+        <a className="link link-hover">Advertisement</a>
+      </nav>
+      <nav>
+        <header className="footer-title">Company</header>
+        <a className="link link-hover">About us</a>
+        <a className="link link-hover">Contact</a>
+        <a className="link link-hover">Jobs</a>
+        <a className="link link-hover">Press kit</a>
+      </nav>
+      <nav>
+        <header className="footer-title">Legal</header>
+        <a className="link link-hover">Terms of use</a>
+        <a className="link link-hover">Privacy policy</a>
+        <a className="link link-hover">Cookie policy</a>
+      </nav>
+    </footer>
+  );
+};
+
+export default Footer;
